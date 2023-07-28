c3bc12e00440450f698097a66b16d609204658a7
Prince Verma <princeverma360p@gmail.com>
2023-07-28 18:15:27 +0530

    deploy commit

--- server.js
+++ server.js
@@ -10,21 +10,20 @@ dotenv.config({ path: "./config.env" });
 //database -------------------------------------------->
 mongoose.set("strictQuery", false);
 mongoose
-    .connect(process.env.DB_URL_CLOUD, {
-        useNewUrlParser: true,
-    })
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch((error) => {
-        console.log(error);
+  .connect(process.env.DB_URL_CLOUD, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+    //server ---------------------------------------------->
+    const port = process.env.PORT || 8000;
+    const server = app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
 
-//server ---------------------------------------------->
-const port = process.env.PORT || 8000;
-const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-//socket ---------------------------------------------->
-sockerHandler.initializeSocketServer(server);
+    //socket ---------------------------------------------->
+    sockerHandler.initializeSocketServer(server);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
